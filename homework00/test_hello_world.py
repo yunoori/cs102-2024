27f1b8d02fb8de1eab4ae42154d1d1e42a686ab4
yunoori <liliakhaibullina0407@gmail.com>
2025-01-22 19:43:26 +0300

    Исправлены ошибки

--- homework00/test_hello_world.py
+++ homework00/test_hello_world.py
@@ -1,10 +1,14 @@
+"""Код для проверки test"""
+
 import unittest
 
 import hello_world
 
 
 class HelloTestCase(unittest.TestCase):
+    """Начало класса"""
+
     def test_hello(self):
+        """Функция по возвращению сообщения и проверке его"""
         m = "message"
         self.assertEqual(m, hello_world.text())
-        
